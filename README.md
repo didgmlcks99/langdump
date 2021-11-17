fe946af2bdc4acaca622eb97efffa7b480f50e45
didgmlcks99 <heechy06@naver.com>
2021-11-17 19:25:54 +0900

    first draft

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# langdump
+# langdump - buildscript
+
+## Inputs
+
+### This program gets a txt file in which holds a recursively defined rule of grammer and an integer value in which limits the member string to be printed to the standard output.
+
+<br>
+
+## Program Description
+
+### This program prints all the member strings of a set of string according to the given recursively defined grammer rule, contianing a length less than the given constraint.
+
+<br>
+
+## Comiling and Run
+* 1. 'make' : run the command, 'make' on the terminal to compile and make a executable file of 'langdump.
+* 2. './langdump \<grammar-description-file> \<max-length>': run the executable file created from step 1 with the command './langdump' including the correct arguments for the program.
+
+<br>
+
+## Input Guide
+* \<grammar-description-file> input must be a .txt file in which defines the rule of grammer of certain set of string recursively.
+
+* \<max-length> input must be a integer between 1 and 64. This integer is the limit in which the length of the printed member strings must satisfy.
+
+<br>
+
+## Makefile
+* This file automatically runs the command for compiling the 'langdump.c' file.
--- langdump.c
+++ langdump.c
@@ -10,17 +10,17 @@ void validate_input(int max_length);
 void start_prog(FILE *file, int max_length);
 char *trim(char *str);
 void trim_quote(char *str);
-char* my_trim(char* str);
+char *my_trim(char *str);
 void print_gram(char **symbols, int symb_count, char ***sequences, int seq_count);
 void gram_recursive(char *target, char **symbols, int symb_count, char ***sequences, int seq_count, int max);
 int symbol_exist(char *target, char **symbols, int symb_count);
 int symbol_index(char *target, char **symbols, int symb_count);
-char* replace_sub(char *target, char *sub, int index);
+char *replace_sub(char *target, char *sub, char *symbol);
 
 int main(int argc, char **args)
 {
-    char *gramFile = args[1];
-    int max_length = atoi(args[2]);
+    char *gramFile = args[argc-2];
+    int max_length = atoi(args[argc-1]);
 
     validate_input(max_length);
     FILE *fp = open_file(gramFile);
@@ -67,19 +67,20 @@ void validate_input(int max_length)
 
 void start_prog(FILE *file, int max_length)
 {
-    char **symbols = (char**)malloc(100 * sizeof(char *));
-    for(int i=0; i<32; i++){
+    char **symbols = (char **)malloc(100 * sizeof(char *));
+    for (int i = 0; i < 32; i++)
+    {
         symbols[i] = (char *)malloc(16 * sizeof(char));
     }
     int symb_count = 0;
 
-    char*** sequences = (char***)malloc(100 * sizeof(char**));
+    char ***sequences = (char ***)malloc(100 * sizeof(char **));
     for (int i = 0; i < 32; i++)
     {
-        sequences[i] = (char**)malloc(32 * sizeof(char*));
+        sequences[i] = (char **)malloc(32 * sizeof(char *));
         for (int j = 0; j < 3; j++)
         {
-            sequences[i][j] = (char*)malloc(64 * sizeof(char));
+            sequences[i][j] = (char *)malloc(64 * sizeof(char));
         }
     }
     int seq_count = 0;
@@ -90,7 +91,8 @@ void start_prog(FILE *file, int max_length)
         char *token = strtok(line, ":==");
         token = trim(token);
 
-        if(token[0] == 'S') {
+        if (token[0] == 'S')
+        {
             token = strtok(NULL, ":==");
             token = trim(token);
             trim_quote(token);
@@ -98,7 +100,9 @@ void start_prog(FILE *file, int max_length)
 
             strcpy(symbols[symb_count], token);
             symb_count++;
-        }else{
+        }
+        else
+        {
             strcpy(sequences[seq_count][0], token);
 
             token = strtok(NULL, ":==");
@@ -112,7 +116,7 @@ void start_prog(FILE *file, int max_length)
         }
     }
 
-    print_gram(symbols, symb_count, sequences, seq_count);
+    // print_gram(symbols, symb_count, sequences, seq_count);
 
     gram_recursive(symbols[0], symbols, symb_count, sequences, seq_count, max_length);
 }
@@ -122,113 +126,137 @@ char *trim(char *str)
 {
     char *end;
 
-    while(isspace((unsigned char)*str)) str++;
+    while (isspace((unsigned char)*str))
+        str++;
 
-    if(*str == 0)
+    if (*str == 0)
         return str;
 
     end = str + strlen(str) - 1;
-    while(end > str && isspace((unsigned char)*end)) end--;
+    while (end > str && isspace((unsigned char)*end))
+        end--;
 
     end[1] = '\0';
 
     return str;
 }
 
-void trim_quote(char *str){
-    for(int i=0; i<(int)strlen(str); i++){
-        if(str[i] == '"') str[i] = ' ';
+void trim_quote(char *str)
+{
+    for (int i = 0; i < (int)strlen(str); i++)
+    {
+        if (str[i] == '"')
+            str[i] = ' ';
     }
 }
 
-char* my_trim(char* str)
+char *my_trim(char *str)
 {
     static char trimmed[64];
     int trimmed_count = 0;
 
-    for(int i=0; i<(int)strlen(str); i++){
-        if(str[i] != 32){
+    for (int i = 0; i < (int)strlen(str); i++)
+    {
+        if (str[i] != 32)
+        {
             trimmed[trimmed_count] = str[i];
             trimmed_count++;
         }
     }
 
     trimmed[trimmed_count] = '\0';
-  
+
     return trimmed;
 }
 
-void print_gram(char **symbols, int symb_count, char ***sequences, int seq_count){
+void print_gram(char **symbols, int symb_count, char ***sequences, int seq_count)
+{
     printf("Symbols\n");
-    for(int i=0; i<symb_count; i++){
+    for (int i = 0; i < symb_count; i++)
+    {
         printf("%c\n", symbols[i][0]);
     }
 
     printf("\nSymbols : Expressions\n");
-    for(int i=0; i<seq_count; i++){
+    for (int i = 0; i < seq_count; i++)
+    {
         printf("%c : %s\n", sequences[i][0][0], sequences[i][1]);
     }
 
     printf("\n===========================\n\n");
 }
 
-void gram_recursive(char *target, char **symbols, int symb_count, char ***sequences, int seq_count, int max){
-    if((int)strlen(target) > max){
-    }else if(symbol_exist(target, symbols, symb_count) == 0){
+void gram_recursive(char *target, char **symbols, int symb_count, char ***sequences, int seq_count, int max)
+{
+    if ((int)strlen(target) > max)
+    {
+        return;
+    }
+    else if (symbol_exist(target, symbols, symb_count) == 0)
+    {
         printf("%s\n", target);
-    }else{
-        int index = symbol_index(target, symbols, symb_count);
-        char *send = (char*)malloc(64 * sizeof(char));
+    }
+    else
+    {
+        char *send = (char *)malloc(64 * sizeof(char));
 
-        for(int i=0; i<seq_count; i++){
-            if(target[index] == sequences[i][0][0]){
-                if((int)strlen(target) == 1){
-                    send = sequences[i][1];
-                }else{
-                    send = replace_sub(target, sequences[i][1], index);
-                }
+        char *temp;
+
+        for (int i = 0; i < symb_count; i++)
+        {
+            if (strstr(target, symbols[i]) != NULL)
+            {
+                temp = symbols[i];
+                break;
+            }
+        }
+
+        for (int i = 0; i < seq_count; i++)
+        {
+            if (strstr(sequences[i][0], temp) != NULL)
+            {
+                send = replace_sub(target, sequences[i][1], sequences[i][0]);
                 gram_recursive(send, symbols, symb_count, sequences, seq_count, max);
             }
         }
     }
 }
 
-int symbol_exist(char *target, char **symbols, int symb_count){
-    for(int i=0; i<symb_count; i++){
-        for(int j=0; j<(int)strlen(target); j++){
-            if(target[j] == symbols[i][0]) return 1;
-        }
-    }
-    return 0;
-}
-
-int symbol_index(char *target, char **symbols, int symb_count){
-    for(int i=0; i<symb_count; i++){
-        for(int j=0; j<(int)strlen(target); j++){
-            if(target[j] == symbols[i][0]) return j;
-        }
-    }
-    return 0;
-}
-
-char* replace_sub(char *target, char *sub, int index)
+int symbol_exist(char *target, char **symbols, int symb_count)
 {
-    char *result = (char*)malloc(64 * sizeof(char));
+    for (int i = 0; i < symb_count; i++)
+    {
+        if (strstr(target, symbols[i]) != NULL)
+            return 1;
+    }
+    return 0;
+}
+
+char *replace_sub(char *target, char *sub, char *symbol)
+{
+    char *result = (char *)malloc(64 * sizeof(char));
 
     int sub_len = (int)strlen(sub);
+    int sym_len = (int)strlen(symbol);
 
-    for(int i=0; i<index; i++){
-        result[i] = target[i];
-    }
+    char *test = (char *)malloc(64 * sizeof(char));
+    test = target;
 
-    strcat(result, sub);
-
-    int x = index+1;
-    int y = index+sub_len;
-    while(x < (int)strlen(target)){
-        result[y] = target[x];
-        x++;
-        y++;
+    int i = 0;
+    while (*test)
+    {
+        if (strstr(test, symbol) == test)
+        {
+            strcpy(&result[i], sub);
+            i += sub_len;
+            test += sym_len;
+            strcat(result, test);
+            break;
+        }
+        else
+        {
+            result[i++] = *test++;
+        }
     }
 
     return result;
